ae07b194fed83132fcb5a8dfaa881efe9042ea2e
David Wu <davidcnwu@gmail.com>
2014-02-01 19:59:03 -0800

    Working on Menu

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ A simple gem that displays an emoji menu
 
 Trying to make the awesome http://www.disapprovallook.com/ accessible by command line.
 
+```javascript
+$.map($('.postbody p'), function(node) { return node.textContent; } ).join("\n")
+```
+
 ```ruby
 rake install
 emoji
--- lib/emoji.rb
+++ lib/emoji.rb
@@ -1,9 +1,34 @@
 require "emoji/version"
 
 module Emoji
-  class Chatter
-    def say_hello
-      puts 'This is zerp. Coming in loud and clear. Over.'
+  class Menu
+    def initialize
+      @emojis = Emojis.new
+    end
+
+    def run
+      print_list
+      get_input
+      pbcopy_emoji
+    end
+
+    def print_list
+      @emojis.list.each_with_index do |item, index|
+        puts "%3d: %5s" % [index, item]
+      end
+    end
+
+    def get_input
+    end
+
+    def pbcopy_emoji
+    end
+  end
+
+  class Emojis
+    def list
+      # TODO: Fix this so it works wherever the data is stored
+      @emojis ||= File.read("data/emoji.dat").split("\n")
     end
   end
 end
