13421908270f6b79f6658aef0e1f32b569a3b030
David Wu <davidcnwu@gmail.com>
2014-02-03 21:18:57 -0800

    Basic working version.

--- lib/emoji.rb
+++ lib/emoji.rb
@@ -2,27 +2,40 @@ require "emoji/version"
 
 module Emoji
   class Menu
-    def initialize
-      @emojis = Emojis.new
+    attr_reader :per_line, :emojis
+
+    def initialize(options = {})
+      @emojis = Emojis.new.list
+      @per_line = options[:per_line] || 1
     end
 
     def run
       print_list
-      get_input
-      pbcopy_emoji
+      input = get_input
+      pbcopy_emoji(input)
     end
 
     def print_list
-      @emojis.list.each_with_index do |item, index|
-        puts "%3d: %5s" % [index, item]
+      emojis.each_with_index do |item, index|
+        print "%3d: %s" % [index, item]
+        puts if ((index + 1) % per_line).zero?
       end
     end
 
     def get_input
+      prompt("Select One: ").to_i
     end
 
-    def pbcopy_emoji
+    def pbcopy_emoji(input)
+      emoji = emojis[input]
+      %x{/bin/bash -c "echo -n '#{emoji}' | pbcopy"}
     end
+
+    private
+      def prompt(input)
+        print input
+        gets.strip
+      end
   end
 
   class Emojis
